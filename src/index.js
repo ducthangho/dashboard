4f3b93045f4315bf46d4e147f70132b2ee8dc419
ducthangho <ducthangho@gmail.com>
2019-03-19 10:44:34 +0700

    The rest

--- src/index.js
+++ src/index.js
@@ -1,12 +1,42 @@
-import React from 'react'
+import React, {Component} from 'react'
 import ReactDOM from 'react-dom'
 //import Message from './js/Message'
-import App from './js/App';
 
-import './css/style.css'
+import Header from './js/Header';
+import Sidebar from './js/Sidebar';
+import Content from './js/Content';
+// import Footer from './js/Footer';
+import SidebarCtrl from './js/SidebarCtrl';
+
+
+//import jQuert from 'jquery';
+import 'bootstrap';
+import 'jquery-slimscroll';
+import 'admin-lte';
+
+ //import '../dist/styles.css'
+import './css/bootstrap.min.css'
+import './css/AdminLTE.css'
+import './css/skins/_all-skins.min.css'
+import './css/ionicons.min.css'
+import './css/font-awesome.min.css'
+
+class MyComponent extends Component {  
+  render() {    
+    return (            
+      <div>
+        <Header />
+        <Sidebar />
+        <Content />        
+        <SidebarCtrl />        
+      </div>   
+    );
+  }
+}
+
 
 ReactDOM.render(
-  <App />,
+  <MyComponent />,
   document.getElementById('root') // eslint-disable-line no-undef
 )
 
