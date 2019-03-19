2ad50227ae714b1813659df1a3628f6e72c8d004
ducthangho <ducthangho@gmail.com>
2019-03-19 10:33:38 +0700

    Update config

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,8 +1,26 @@
 const path = require('path')
 const webpack = require('webpack')
 const HtmlWebPackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+const devMode = process.env.NODE_ENV !== 'production'
 
 module.exports = {
+
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        styles: {
+          name: 'styles',
+          test: /\.css$/,          
+          chunks: 'all',
+          minChunks: 1,
+          reuseExistingChunk: true,
+          enforce: true
+        }
+      }
+    }
+  },
+
   entry: {
     main: ['webpack-hot-middleware/client?path=/__webpack_hmr&timeout=20000', './src/index.js']
   },
@@ -54,22 +72,52 @@ module.exports = {
         ]
       },
       { 
-        test: /\.css$/,
-        use: [ 'style-loader', 'css-loader' ]
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          'style-loader',
+          MiniCssExtractPlugin.loader,
+          { loader: 'css-loader', options: { url: true, sourceMap: true } },
+          { loader: 'sass-loader', options: { sourceMap: true } }        
+        ]
+        // test: /\.css$/,
+        // use: [ 'style-loader', 'css-loader' ]
       },
       {
-       test: /\.(png|svg|jpg|gif)$/,
-       use: ['file-loader']
+        test: /\.(woff|woff2)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader?mimetype=application/font-woff" ,
+      },
+      { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader" },
+      {
+       test: /\.(png|svg|jp(e*)g|gif)$/,
+       use: [
+          {
+              loader: 'url-loader',
+              options: { 
+                  limit: 8192, // Convert images < 8kb to base64 strings
+                  fallback: 'file-loader'
+              }         
+          }
+       ]
       }
     ]
   },
   plugins: [
+    new webpack.ProvidePlugin({
+          jQuery : 'jquery',
+          'window.jQuery': 'jquery',
+          'window.$': 'jquery',
+    }),
     new HtmlWebPackPlugin({
       template: "./src/html/index.html",
       filename: "./index.html",
       excludeChunks: [ 'server' ]
-    }),
+    }), 
     new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoEmitOnErrorsPlugin()
+    new webpack.NoEmitOnErrorsPlugin(),
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional      
+      filename: devMode ? '[name].css' : '[name].[hash].css',
+      chunkFilename: devMode ? '[id].css' : '[id].[hash].css',
+    })
   ]
 }
